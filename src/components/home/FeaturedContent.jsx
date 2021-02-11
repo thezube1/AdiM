b57194d09b187996c944595379b179cc1825ef2d
Zubin Hydrie <zubin.hydrie@bush.edu>
2021-02-10 19:33:06 -0800

    Added feature

--- src/components/home/FeaturedContent.jsx
+++ src/components/home/FeaturedContent.jsx
@@ -1,17 +1,65 @@
 import React from "react";
 import "./featuredContent.css";
-import { Bounce } from "react-reveal";
+import { Fade, Zoom } from "react-reveal";
+import { motion } from "framer-motion";
+
+const lineExtend = {
+  rest: {
+    width: "0%",
+    transition: {
+      duration: 1,
+    },
+  },
+  hover: {
+    width: "100%",
+    transition: {
+      duration: 1,
+    },
+  },
+};
 
 const FeaturedContent = () => {
   return (
     <div className="featuredWrapper">
       <div className="featuredContent">
-        <Bounce left>
-          <div id="featuredTitle">
-            Summer <br />
-            Daydream
-          </div>
-        </Bounce>
+        <div style={{ maxWidth: "max-content" }}>
+          <Fade left>
+            <div id="featuredTitle">
+              Summer <br />
+              Daydream
+            </div>
+          </Fade>
+          <Zoom cascade>
+            <div id="featuredBar"></div>
+          </Zoom>
+        </div>
+        <div
+          className="featuredHeader"
+          style={{ fontSize: 20, marginTop: 10, color: "rgb(247, 199, 203)" }}
+        >
+          Out now
+        </div>
+        <motion.a
+          variants={lineExtend}
+          href="https://ffm.to/summerdaydream"
+          className="featuredHeader"
+          target="_blank"
+          rel="noreferrer"
+          id="featuredListen"
+        >
+          <motion.div
+            initial="rest"
+            whileHover="hover"
+            animate="rest"
+            style={{ width: 300 }}
+          >
+            <div style={{ width: 300 }}>Listen</div>
+            <motion.div
+              variants={lineExtend}
+              className="featuredItemUnder"
+            ></motion.div>
+          </motion.div>
+        </motion.a>
       </div>
     </div>
   );
